56ae9ed571ddacea8be1f762ca17e5fb5a5911ac
Michael Claybaugh <michael.claybaugh@gmail.com>
2020-03-02 11:47:05 -0500

    explain focus-within.js

--- src/focus-within.js
+++ src/focus-within.js
@@ -1,3 +1,6 @@
+/**
+ * Adds class "focus-within" to elements like a pseudo-selector
+ */
 var topItems = document.querySelectorAll('#zone-menu-wrapper .main-menu ul.menu li.level-1');
 Array.prototype.forEach.call(topItems, function (el) {
   el.addEventListener('focusin', function () {
